d9320a78132ef7f06b5bbcb907ed1c9133aa165d
Alan deLevie <adelevie@gmail.com>
2016-03-30 23:13:18 -0400

    Add anchorjs

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
   hljs.initHighlightingOnLoad();
+  anchors.options = {
+    visible: 'always',
+    placement: 'left',
+    icon: '¶'
+  };
+  anchors.add('p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6');
 });
-
