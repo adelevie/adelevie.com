755d5cbdf26ea06bb35c16626858bdb9f4bb8260
Alan deLevie <adelevie@gmail.com>
2016-03-30 23:19:23 -0400

    tweak anchorjs

--- js/main.js
+++ js/main.js
@@ -1,9 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
   hljs.initHighlightingOnLoad();
-  anchors.options = {
-    visible: 'always',
-    placement: 'left',
-    icon: '¶'
-  };
-  anchors.add('p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6');
+  anchors.add();
 });
